2c9a5bd59bf55297d93359db7bb17162f9ea7af9
Shruti Jayaswal <shrutijayaswal671@gmail.com>
2026-01-08 01:54:03 +0530

    early access link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,7 +42,7 @@ const Footer = () => {
           <div className="lg:col-span-2 flex flex-col">
             <Link href="/" onClick={handleLinkClick} className="mb-6">
               <Image
-                src="/MetaWurks Logo-11.svg"
+                src="/Untitled design.svg"
                 alt="MetaWurks Logo"
                 width={220}
                 height={55}
