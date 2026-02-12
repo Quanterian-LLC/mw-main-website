57ed7c8ba2187726fd65269f58cc0ece52099a63
Shruti Jayaswal <shrutijayaswal671@gmail.com>
2026-02-12 20:06:08 +0530

    add favicon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,7 +42,7 @@ const Footer = () => {
           <div className="lg:col-span-2 flex flex-col">
             <Link href="/" onClick={handleLinkClick} className="mb-6">
               <Image
-                src="/Untitled design.svg"
+                src="/logo.png"
                 alt="MetaWurks Logo"
                 width={220}
                 height={55}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
             <Image
-              src="/Untitled design.svg"
+              src="/logo.png"
               alt="MetaWurks Logo"
               width={200}
               height={50}
